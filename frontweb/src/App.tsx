5ec2ad41f8a81183496fd2cc4373d22a3b7196a3
nevesade <adenasiocarvalho19@gmail.com>
2022-09-12 20:21:55 +0100

    Começando o componente Navbar

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,9 +1,11 @@
+import Navbar from 'components/Navbar';
 import './App.css';
 import './assets/styles/custom.scss';
 
 function App() {
   return (
     <div >
+      <Navbar/>
       <h1>Hello Dev</h1>
     </div>
   );

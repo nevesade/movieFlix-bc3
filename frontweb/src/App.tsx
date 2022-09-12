40fcd168fd70a093f6f00c98a5610bf38f88e4c6
nevesade <adenasiocarvalho19@gmail.com>
2022-09-12 22:52:23 +0100

    banner e css

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,11 +1,11 @@
 import './App.css';
 import './assets/styles/custom.scss';
-import Navbar from 'components/Navbar';
+import Home from 'pages/Home';
 
 function App() {
   return (
     <>
-      <Navbar />
+      <Home />
       
     </>
   );
---
+++ frontweb/src/pages/Home/index.tsx
@@ -0,0 +1,26 @@
+import {ReactComponent as MainImage} from 'assets/images/main-image.svg';
+import './styles.css';
+
+import Navbar from "components/Navbar";
+
+const Home = () => {
+    return (
+      <>
+        <Navbar />
+        <div className="home-container container">
+          <div className="banner-card">
+              <div className='banner-content-container'>
+                  <h1>Avalie Filmes</h1>
+                  <p>Diga o que você achou do seu filme favorito</p>
+  
+              </div>
+              <div className='banner-image-container'>
+                  <MainImage/>
+              </div>
+          </div>
+        </div>
+      </>
+    );
+  };
+  
+  export default Home;

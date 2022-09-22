33f23c862fc0693b2b1941e68db5baecd96a0702
nevesade <adenasiocarvalho19@gmail.com>
2022-09-22 10:35:32 +0100

    HTML do card de Login

--- frontweb/src/pages/Home/Login/index.tsx
+++ frontweb/src/pages/Home/Login/index.tsx
@@ -1,21 +1,40 @@
-import { useForm } from "react-hook-form";
+
+
+ 
 import './styles.css';
  
 const Login = () => {
   return (
-
-    <>
-
-<div className="base-card form-container">
+    <div className="base-card login-card">
       <h1>LOGIN</h1>
-      
-    
+      <form>
+        <div className="mb-4">
+          <input
+            type="text"
+            className="form-control base-input"
+            placeholder="Email"
+            name="username"
+          />
+        </div>
+        <div className="mb-2">
+          <input
+            type="password"
+            className="form-control base-input "
+            placeholder="Password"
+            name="password"
+          />
+        </div>
+        
+        <div className="login-submit">
+          <button>Fazer login</button>
+        </div>
+        
+      </form>
     </div>
-      
-       
-    </>
-   
   );
 };
  
 export default Login;
+
+      
+    

a99b93095b0c0554d65811d836cb2fc53e72bd86
nevesade <adenasiocarvalho19@gmail.com>
2022-10-07 22:06:30 +0100

    Validação de dados com React Hook Form

--- frontweb/src/pages/Home/Login/index.tsx
+++ frontweb/src/pages/Home/Login/index.tsx
@@ -25,7 +25,7 @@ const Login = () => {
 
   const history = useHistory();
 
-  const { register, handleSubmit } = useForm<FormData>();
+  const { register, handleSubmit, formState: {errors} } = useForm<FormData>();
 
   const onSubmit = (formData: FormData) => {
 
@@ -54,21 +54,31 @@ const Login = () => {
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="mb-4">
           <input
-            {...register('username')}
+            {...register('username', {
+              required: 'Campo obrigátorio'
+            })}
             type="text"
             className="form-control base-input"
             placeholder="Email"
             name="username"
           />
+            <div className="invalid-feedback d-block">
+            {errors.username?.message}
+          </div>
         </div>
         <div className="mb-2">
           <input
-            {...register('password')}
+            {...register('password',  {
+              required: 'Campo obrigátorio'
+            })}
             type="password"
             className="form-control base-input "
             placeholder="Password"
             name="password"
           />
+           <div  className="invalid-feedback d-block">
+            {errors.password?.message}
+          </div>
         </div>
 
         <div className="login-submit">

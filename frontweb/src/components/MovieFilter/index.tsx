5d22b2267bcda7575618ab05c3159fb29521e373
nevesade <adenasiocarvalho19@gmail.com>
2022-11-15 19:10:10 +0000

    Filtrar Filme por gêneros

--- frontweb/src/components/MovieFilter/index.tsx
+++ frontweb/src/components/MovieFilter/index.tsx
@@ -1,32 +1,84 @@
+import { AxiosRequestConfig } from 'axios';
+import { useEffect, useState } from 'react';
+import { Controller, useForm } from 'react-hook-form';
 import Select from 'react-select';
+import { Genre } from 'types/genre';
+import { requestBackend } from 'utils/requests';
 import './styles.css';
 
+export type MovieFilterData = {
+  name: string;
+  genre: Genre | null;
+};
+
+type Props = {
+  onSubmitFilter: (data: MovieFilterData) => void;
+};
+
+const MovieFilter = ({ onSubmitFilter }: Props) => {
+  const [selectGenre, SetSelectGenre] = useState<Genre[]>([]);
+
+  const {  handleSubmit, setValue, getValues, control } =
+    useForm<MovieFilterData>();
+
+  const onSubmit = (formData: MovieFilterData) => {
+    onSubmitFilter(formData);
+    // console.log('Enviou', formData);
+  };
 
 
-const MovieFilter = () => {
+  const handleChangeGenre = (value: Genre) => {
+    setValue('genre', value);
 
-      const options = [
-    { value: 'Aventura', label: 'Aventura' },
-    { value: 'Comedia', label: 'Comedia' },
-    { value: 'Terror', label: 'Terror' }
-  ]
+    const obj = {
+      name: getValues('name'),
+      genre: getValues('genre'),
+    };
 
-    return(
-        <div className="  conatainer filter ">
-                <div className="filter-select">
-               <Select
-                options={options}
+    onSubmitFilter(obj);
+    // console.log("Enviou", obj)
+  };
+
+    const params: AxiosRequestConfig = {
+    method: 'GET',
+    url: '/genres',
+    withCredentials: true,
+  };
+ 
+   useEffect(() => {
+    requestBackend(params).then((response) => {
+      SetSelectGenre(response.data);
+      console.log(response.data);
+    });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []); 
+ 
+
+  return (
+    <div className="  conatainer filter ">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="filter-select">
+          <Controller
+            name="genre"
+            control={control}
+            render={({ field }) => (
+              <Select
+                {...field}
+                options={selectGenre}
                 classNamePrefix="movie-filter-select"
                 isClearable
-                placeholder="Aventura"
-               />
-
-              </div>
+                placeholder="Filtrar por Gênero"
+                getOptionLabel={(genre: Genre) => genre.name}
+                getOptionValue={(genre: Genre) => String(genre.id)}
+                onChange={(value) => handleChangeGenre(value as Genre)}
+              />
+            )}
+          />
+    
         </div>
-    )
-
-
-}
+      </form>
+    </div>
+  );
+};
 
 export default MovieFilter;
-
--- frontweb/src/pages/Private/MoviesCatalog/index.tsx
+++ frontweb/src/pages/Private/MoviesCatalog/index.tsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { AxiosRequestConfig } from 'axios';
 import MovieCard from 'components/MovieCard';
-import MovieFilter from 'components/MovieFilter';
+import MovieFilter, { MovieFilterData } from 'components/MovieFilter';
 import Pagination from 'components/Pagination';
 import { Movie } from 'types/movie';
 import { requestBackend } from 'utils/requests';
@@ -9,27 +9,54 @@ import './styles.css';
 import {SpringPage} from 'types/vendor/spring';
 import { Link } from 'react-router-dom';
 
+
+type ControlComponentsData =  {
+  activePage: number;
+  filterData: MovieFilterData;
+}
+
+
 const MovieCatalog = () => {
 
   const [page, setPage] = useState<SpringPage<Movie>>();
 
+  const [controlComponentsData, setControlComponentsData] = useState<ControlComponentsData>( 
+    {
 
-  const getMovies = (pageNumber : number) =>{
+        activePage: 0,
+        filterData: {name: "", genre: null},
 
-    const params: AxiosRequestConfig = {
+    }
+);
+
+const handlePageChange = (pageNumber : number) => {
+  setControlComponentsData({activePage: pageNumber, filterData:  controlComponentsData.filterData});
+};
+
+const handleSubmitFilter = (data: MovieFilterData) => {
+  setControlComponentsData({activePage: 0, filterData: data });
+}
+
+
+
+  const getMovies = useCallback(() => {
+
+    const config: AxiosRequestConfig = {
       method: 'GET',
       url: "/movies",
       withCredentials: true,
       params: {
-        page: pageNumber,
+        page: controlComponentsData.activePage,
         size: 4,
+        name: controlComponentsData.filterData.name,
+        genreId: controlComponentsData.filterData.genre?.id
       },
 
 
     };
 
     
-    requestBackend(params).then((response) => {
+    requestBackend(config).then((response) => {
       setPage(response.data);
       console.log(page);
     }).finally(() => {
@@ -38,11 +65,13 @@ const MovieCatalog = () => {
 
     )
 
-  }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[controlComponentsData]);
 
   useEffect(() => {
-   getMovies(0)
-  }, []);
+   getMovies()
+  
+  }, [getMovies]);
 /*
   
 
@@ -72,7 +101,7 @@ const MovieCatalog = () => {
     <>
       <div className=" container my-4  ">
       
-        <MovieFilter />
+        <MovieFilter onSubmitFilter={handleSubmitFilter} />
        
 
         <div className="row container ">
@@ -100,7 +129,7 @@ const MovieCatalog = () => {
         <Pagination 
             pageCount={(page) ? page.totalPages : 0}
             range= {3} 
-            onChange={getMovies}
+            onChange={handlePageChange}
             />
         </div>
       </div>
